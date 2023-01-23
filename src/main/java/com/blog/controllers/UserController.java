040a2b0ba846d3db488e4bfdc6222ff17e6d95b8
Snehashis Dasgupta <snehashis.jisu.cse@gmail.com>
2023-01-23 12:46:30 +0500

    Added validating data using Bean validator

--- src/main/java/com/blog/controllers/UserController.java
+++ src/main/java/com/blog/controllers/UserController.java
@@ -1,15 +1,14 @@
 package com.blog.controllers;
 
-import com.blog.payloads.ApiResponse;
-import com.blog.payloads.UserDto;
+import com.blog.payloads.*;
 import com.blog.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/api/users")
@@ -19,14 +18,14 @@ public class UserController {
 
     // POST- create user
     @PostMapping("/")
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto){
         UserDto createUserDto = this.userService.createUser(userDto);
         return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
     }
 
     // PUT- update user
     @PutMapping("/{userId}")
-    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("userId") Integer uid){
+    public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto userDto, @PathVariable("userId") Integer uid){
         UserDto updatedUser= this.userService.updateUser(userDto, uid);
         return ResponseEntity.ok(updatedUser);
     }
--- src/main/java/com/blog/payloads/UserDto.java
+++ src/main/java/com/blog/payloads/UserDto.java
@@ -1,5 +1,6 @@
 package com.blog.payloads;
 
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @NoArgsConstructor
@@ -8,8 +9,18 @@ import lombok.*;
 public class UserDto {
 
     private int id;
+
+    @NotEmpty
+    @Size(min=3, message = "Username must have 3 characters")
     private String name;
+
+    @Email(message = "Invalid email-address")
     private String email;
+
+    @NotEmpty
+    @Size(min = 3, message = "Password must be min of 3 characters")
     private String password;
+
+    @NotEmpty
     private String about;
 }
